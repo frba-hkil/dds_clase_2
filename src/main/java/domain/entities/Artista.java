4d6a4b5ab60eab11551f1c20aa4c34f3fdcc541b
tomas <khysnc@gmail.com>
2025-04-01 12:27:19 -0300

    atributos iniciales cancion

--- src/main/java/domain/entities/Artista.java
+++ src/main/java/domain/entities/Artista.java
@@ -1,4 +1,5 @@
 package domain.entities;
 
 public class Artista {
+
 }
--- src/main/java/domain/entities/Cancion.java
+++ src/main/java/domain/entities/Cancion.java
@@ -1,4 +1,31 @@
 package domain.entities;
 
+import java.time.LocalDateTime;
+
 public class Cancion {
+    private String nombre;
+    private Album album;
+    private Integer cantLikes;
+    private Integer cantDislikes;
+    private Integer anioLanzamiento;
+    private Integer cantReproduccion;
+    private LocalDateTime ultimaReproduccion;
+    //private ??? popularidad;
+
+    public Cancion(String nombre, Album album, Integer anioLanzamiento) {
+        this.cantReproduccion = 0;
+        this.cantLikes = 0;
+        this.cantDislikes = 0;
+        this.nombre = nombre;
+        this.album = album;
+        this.anioLanzamiento = anioLanzamiento;
+    }
+
+    public String serEscuchadada() {
+        this.cantReproduccion++;
+        this.ultimaReproduccion = LocalDateTime.now();
+
+        //TODO: devolver detalle dependiendo de la popularidad
+        return "Detalle completo";
+    }
 }
